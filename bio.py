a662ed744599d2e2d36cf8f4210d23e694c4109f
JBarillas <jbarillas2015ig@gmail.com>
2019-10-03 07:12:17 -0600

    Eliminating dummy data
    Eliminating data that is not usefull for the project

--- bio.py
+++ bio.py
@@ -6,30 +6,6 @@ app = Flask(__name__)
 developer = os.getenv("Developer", "Juan Barillas")
 environment = os.getenv("ENVIRONMENT", "development")
 
-alumnos = {
-    "Andrea Reyes": 19,
-    "Katherine Garcia": 17,
-    "Fabricio Juarez": 18,
-    "Jean Mejicanos": 17,
-    "Steven Wilson": 19,
-    "Ian Jenatz": 19,
-    "Anesbeth Maatens": 17,
-    "Tirso Cordova": 20,
-    "Abner Xocop": 21,
-    "Andres Bolanos": 21,
-    "Katherin Mazariegos": 19,
-    "Daniel Hernandez": 20,
-    "Maite de la Roca": 20,
-    "Diego Quan": 22,
-    "Fernando Gonzalez": 20,
-    "Boris Rendon": 19,
-    "Adriana Mundo": 20,
-    "Alejandra Lemus": 19,
-    "Juan Barillas": 21,
-    "David Corzo": 19,
-    "Giovanny Telon": 25
-}
-
 @app.route("/")
 def home():
     foo="bar"
@@ -37,7 +13,11 @@ def home():
 
 @app.route("/Academic Information")
 def api_students():
-    return jsonify(alumnos)
+    pass
+
+@app.route("/Work Experience")
+def falta():
+    pass
 
 if __name__ == "__main__":
     debug=False
